73b8bb1bcf0fc22d830dc94abb96601059ea0078
Jess Latimer <jplatimer@gmail.com>
2014-08-28 13:14:13 -0700

    updating for 0.9.0

--- package.js
+++ package.js
@@ -1,8 +1,15 @@
 Package.describe({
 	summary: "Stand-alone encryption package to encrypt/decrypt data with AES using Crypto-JS."
+	git: "https://github.com/manybothans/Meteor-AES.git",
+    version: "1.1.0",
+    name: "jplatimer:aes",
 });
 
-Package.on_use(function (api) {
-	api.add_files('aes.js', ['client', 'server']);
+Package.onUse(function (api) {
+	// If no version is specified for an 'api.use' dependency, use the
+	// one defined in Meteor 0.9.0.
+	api.versionsFrom('0.9.0');
+
+	api.addFiles('aes.js');
 	api.export && api.export('CryptoJS');
 });
